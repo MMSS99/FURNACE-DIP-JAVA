8200059536b2216d177823aab1730992e182f504
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-04-11 16:25:01 +0200

    refactor(Room): :recycle: changed constructor to not recieve any value

--- src/main/java/edu/estatuas/furnace/rooms/Room.java
+++ src/main/java/edu/estatuas/furnace/rooms/Room.java
@@ -1,16 +1,18 @@
 package edu.estatuas.furnace.rooms;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Room {
     private double temperature;
     private static Room room = null;
 
-    private Room(double initialTemperature){
-        temperature = initialTemperature;
+    private Room(){
+        temperature = ThreadLocalRandom.current().nextInt(0, 22);;
     }
 
-    public static Room getInstance(double initialTemperature){
+    public static Room getInstance(){
         if (room == null){
-            room = new Room(initialTemperature);
+            room = new Room();
         }
         return room;
     }
